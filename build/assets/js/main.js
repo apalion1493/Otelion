18c75654071f692a4b1247f668e833c051602951
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2019-09-29 10:53:55 +0300

    commit 29

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,1 +1,49 @@
-function openCity(e,t){var n,a,l;for(a=document.getElementsByClassName("tabcontent"),n=0;n<a.length;n++)a[n].style.display="none";for(l=document.getElementsByClassName("tablinks"),n=0;n<l.length;n++)l[n].className=l[n].className.replace(" active","");document.getElementById(t).style.display="block",e.currentTarget.className+=" active"}jQuery(document).ready(function(){jQuery(".menu-burger").click(function(){jQuery(this).toggleClass("active")})});
+// = jquery/jquery.min.js
+// = bootstrap/bootstrap.min.js
+// = wow/wow.min.js
+
+
+jQuery(document).ready(function(){
+    jQuery('.menu-burger').click(function () {
+        jQuery(this).toggleClass('active');
+    });
+
+    $('#search-scheme__input').focus(function(){
+        $(this).parent().addClass('active');
+    });
+    $('#search-scheme__input').focusout(function(){
+        $(this).parent().removeClass('active');
+    });
+
+    jQuery('.search-scheme__choice').click(function () {
+        jQuery('.search-scheme__dropdown').toggleClass('active');
+    });
+});
+
+jQuery(function($){
+    $(document).mouseup(function (e){ // событие клика по веб-документу
+        let div1 = $(".search-scheme__choice");
+        let div2 = $(".search-scheme__dropdown");// тут указываем ID элемента
+        if (!div1.is(e.target) // если клик был не по нашему блоку
+            && div1.has(e.target).length === 0
+            && !div2.is(e.target)
+            && div2.has(e.target).length === 0) { // и не по его дочерним элементам
+
+            $('.search-scheme__dropdown').removeClass('active');
+        }
+    });
+});
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,6 +7,31 @@ jQuery(document).ready(function(){
     jQuery('.menu-burger').click(function () {
         jQuery(this).toggleClass('active');
     });
+
+    $('#search-scheme__input').focus(function(){
+        $(this).parent().addClass('active');
+    });
+    $('#search-scheme__input').focusout(function(){
+        $(this).parent().removeClass('active');
+    });
+
+    jQuery('.search-scheme__choice').click(function () {
+        jQuery('.search-scheme__dropdown').toggleClass('active');
+    });
+});
+
+jQuery(function($){
+    $(document).mouseup(function (e){ // событие клика по веб-документу
+        let div1 = $(".search-scheme__choice");
+        let div2 = $(".search-scheme__dropdown");// тут указываем ID элемента
+        if (!div1.is(e.target) // если клик был не по нашему блоку
+            && div1.has(e.target).length === 0
+            && !div2.is(e.target)
+            && div2.has(e.target).length === 0) { // и не по его дочерним элементам
+
+            $('.search-scheme__dropdown').removeClass('active');
+        }
+    });
 });
 
 function openCity(evt, cityName) {
